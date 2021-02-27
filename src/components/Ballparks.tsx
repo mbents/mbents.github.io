1d44689fbafa5ff3c396e0e93e633b1755d76abb
mbents <mtbents@gmail.com>
2021-02-27 00:17:47 -0600

    use MuiDatatables for ballparks

--- src/components/Ballparks.tsx
+++ src/components/Ballparks.tsx
@@ -1,49 +1,103 @@
-import { createStyles, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { useBallparkStore } from '../contexts/BallparkContext'
-import { IBallpark } from '../stores/IBallpark'
-
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    head: {
-      backgroundColor: theme.palette.info.main,
-      color: theme.palette.common.white,
-    }
-  })
-)
+import MUIDataTable from 'mui-datatables'
 
 const Ballparks: React.FC = () => {
   const ballparkStore = useBallparkStore()
-  const classes = useStyles()
+  const { ballparks } = ballparkStore
+
+  const options = {
+    rowsPerPage: 25,
+    rowsPerPageOptions: [25,50,100,200,300],
+    print: false,
+    selectableRows: "none" as const
+  }
+
+  const columns = [
+    {
+      name: "PARKID",
+      label: "Park ID",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: "NAME",
+      label: "Name",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: "AKA",
+      label: "Other Names",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: "CITY",
+      label: "City",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "STATE",
+      label: "State",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "START",
+      label: "Start",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: "END",
+      label: "End",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      name: "LEAGUE",
+      label: "League",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "NOTES",
+      label: "Notes",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    }
+  ]
 
   return (
     <React.Fragment>
-      <h4>B A L L P A R K S</h4>
-      <TableContainer component={Paper}>
-        <Table size="small" aria-label="ballparks table">
-          <TableHead>
-            <TableRow className={classes.head}>
-              <TableCell>Ballpark Name</TableCell>
-              <TableCell>City</TableCell>
-              <TableCell>State</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {ballparkStore.ballparks?.length > 0 ? ballparkStore.ballparks?.map((ballpark: IBallpark, index: number) =>
-            <TableRow key={`${ballpark.AKA}-${index}`}>
-              <TableCell>{ballpark.NAME}</TableCell>
-              <TableCell>{ballpark.CITY}</TableCell>
-              <TableCell>{ballpark.STATE}</TableCell>
-            </TableRow>) :
-            <TableRow>
-              <TableCell colSpan={3}>
-                <Typography variant="h6">No data to display</Typography>
-              </TableCell>
-            </TableRow>}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {ballparks?.length > 0 &&
+      <MUIDataTable
+        title={"Ballparks"}
+        columns={columns}
+        data={ballparks}
+        options={options}
+      />}
     </React.Fragment>
   )
 }
